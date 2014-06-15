75ddbe84b41426f7e0c92583b286b24a491b935c
ralph asendeteufrer <ralphadp@hotmail.com>
2014-06-15 02:36:16 -0400

    New class structure

--- pong.js
+++ pong.js
@@ -1,139 +1,213 @@
-window.addEventListener('load',init,false);
-var canvas=null,ctx=null;
-var PLAYERESTEP=10;
-var ballWidth=10;
-var lastKey1=0;
-var lastKey2=0;
-var dezX=random(10), dezY=(-1)*random(10);
-var player1=new Rectangle(20,200,10,80,001);
-var player2=new Rectangle(770,200,10,80,002);
-var pongBall=new Rectangle(680,200,ballWidth,ballWidth);
+//old handler
+//window.addEventListener('load',init,false);
 
+
+/*Player Position class instanced*/
+var PlayerPosition = new function() {
+    this.LEFT = 1;
+	this.RIGHT = -1;
+}
+
+/*Players Score class instanced*/
+var Score = new function() {
+    this.playerLeft = 0;
+	this.playerRight = 0;
+}
+
+var Random = new function() {
+	
+	this.seed = 10;
+	
+	this.value = function(seed) {
+	    if (seed != null) {
+			this.seed = seed;
+		}
+        return parseInt(Math.random() * this.seed);
+    }
+	
+	this.polarityRandom = function() {
+        var p = Math.random() * 2;
+        if (p > 1) {
+            return 1;
+        }
+        return -1;
+    }
+}
+ 
+/**
+ * Initialize the Game and start it.
+ */ 
+var game = new Game();
+window.init();
+
+/*Window init function*/
 function init(){
- canvas=document.getElementById('canvas');
- canvas.style.background='#000';
- ctx=canvas.getContext('2d');
- run(ctx);
+   if (game.init()) {
+		game.start();
+    }
 }
 
-function run(){
- setTimeout(run, 1000 / 60);
- game();
- paint(ctx);
+/*Game class*/
+function Game() {
+
+    this.init = function() {
+
+        this.canvas = document.getElementById('pongCourt');
+
+        if (this.canvas.getContext) {
+			this.canvas.style.background = '#000';
+			this.context = this.canvas.getContext('2d');
+		    
+			Player.prototype.context = this.context;
+		    Player.prototype.canvasWidth = this.canvas.width;
+			Player.prototype.canvasHeight = this.canvas.height;
+			
+			Ball.prototype.context = this.context;
+			Ball.prototype.canvasWidth = this.canvas.width;
+			Ball.prototype.canvasHeight = this.canvas.height;
+			
+			this.pongBall = new Ball(680, 200, 10);
+			this.player1 = new Player(20, 200, 10, 80, PlayerPosition.LEFT, this.pongBall);
+			this.player2 = new Player(770, 200, 10, 80, PlayerPosition.RIGHT, this.pongBall);
+			
+			return true;
+        }
+		return false;
+	}
+	
+	this.start = function() {
+		this.paint();
+		this.pongBall.draw();
+		this.player1.draw();
+		this.player2.draw();
+        //call the callback-able function :)
+		animate();
+	}
+
+    this.paint = function(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.context.fillText('Player 1 - Score: ' + Score.playerLeft.toString(), 200, 10);
+        this.context.fillText('Player 2 - Score: ' + Score.playerRight.toString(), 500, 10);
+    }
 }
 
-function game(){
-  // Change Direction
-  if(lastKey1==87)
-   player1.y-=PLAYERESTEP;
-  if(lastKey1==83)
-   player1.y+=PLAYERESTEP;
-  if(lastKey2==38)
-   player2.y-=PLAYERESTEP;
-  if(lastKey2==40)
-   player2.y+=PLAYERESTEP;
+/*Player class*/
+function Player(x, y, width, height, position, pongball) {
 
-  // movement restricted for players
-  if(player1.y+80>=canvas.height)
-   player1.y=canvas.height-80;
-  if(player1.y<=0)
-   player1.y=0;
+    Drawable.call(this, x, y, width, height);
+	this.PLAYERESTEP = 10;
+	this.position = position;
+	this.pongBall = pongball;
 
-  if(player2.y+80>=canvas.height)
-   player2.y=canvas.height-80;
-  if(player2.y<=0)
-   player2.y=0;
+ 	this.draw = function() {
+		this.context.fillStyle = '#fff';
+        this.context.fillRect(this.x, this.y, this.width, this.height);
+	};
+	
+	this.move = function () {
+	    if (KEY_STATUS.upPlay || KEY_STATUS.downPlay || KEY_STATUS.down || KEY_STATUS.up) {
+		    // The ship moved, so erase it's current image so it can
+			// be redrawn in it's new location
+			this.context.clearRect(this.x, this.y, this.width, this.height);
+			
+		    // Change Direction
+			if (position == PlayerPosition.LEFT) {
+			    if (KEY_STATUS.upPlay) {
+					this.y -= this.PLAYERESTEP;
+				}
+				if (KEY_STATUS.downPlay) {
+					this.y += this.PLAYERESTEP;
+				}
+			} else {
+				if (KEY_STATUS.up) {
+					this.y -= this.PLAYERESTEP;
+				}
+				if (KEY_STATUS.down) {
+					this.y += this.PLAYERESTEP;
+				}
+			}
+			//restricted movement for players
+			if (this.y + 80 >= this.canvasHeight) {
+			    this.y = this.canvasHeight - 80;
+		    }
+			if (this.y <= 0) {
+				this.y = 0;
+			}
+		}
 
-  if(player1.intersects(pongBall)) {
-     player1.score++;
-     dezX=random(10);
-     pongBall.x += dezX;
-     return;  
-  }
-  if(player2.intersects(pongBall)) {
-     player2.score++;
-     dezX=(-1)*random(10);
-     pongBall.x += dezX;
-     return;
-  }
-    
-  pongBall.x += dezX;
-  pongBall.y += dezY;
-  
-  if( pongBall.x+ballWidth>=canvas.width){
-     dezX=(-1)*random(10);
-     player2.score--;
-  }
-  if(pongBall.y+ballWidth>=canvas.height )
-     dezY=(-1)*random(10);
-  if( pongBall.x<=0){
-     dezX=random(10);
-     player1.score--;
-  }
-  if( pongBall.y<=0)
-     dezY=random(10);
-  
-//  lastKey1=lastKey2=0;
+		if (this.pongBall.intersects(this)) {
+		    this.pongBall.dezX = this.position * Random.value();
+		    this.pongBall.x += this.pongBall.dezX;
+		}
+	    
+	}
 }
+Player.prototype = new Drawable();
 
-function paint(ctx){
- ctx.clearRect(0,0,canvas.width,canvas.height);
+/*Ball class*/
+function Ball(x, y, width) {
 
- ctx.fillStyle='#fff';
- ctx.fillRect(player1.x,player1.y,player1.width,player1.height);
- ctx.fillStyle='#fff';
- ctx.fillRect(pongBall.x,pongBall.y,pongBall.width,pongBall.height);
- ctx.fillStyle='#fff';
- ctx.fillRect(player2.x,player2.y,player2.width,player2.height);
- ctx.fillText('Player 1 score '+player1.score.toString(),200,10);
- ctx.fillText('Player 2 score '+player2.score.toString(),500,10);
+    Drawable.call(this, x, y, width, width);
+	
+	this.draw = function() {
+		this.context.fillStyle = '#fff';
+        this.context.fillRect(this.x, this.y, this.width, this.height);
+	};
+	  
+	this.move = function() {
+		
+	  this.x += this.dezX;
+	  this.y += this.dezY;
+	  
+	  if (this.x + this.width >= this.canvasWidth) {
+		 this.dezX = PlayerPosition.RIGHT * Random.value();
+		 Score.playerRight--;
+	  }
+	  if (this.y + this.width >= this.canvasHeight ) {
+		 this.dezY = PlayerPosition.RIGHT * Random.value();
+      }
+	  if (this.x <= 0) {
+		 this.dezX = PlayerPosition.LEFT * Random.value();
+		 Score.playerLeft--;
+	  }
+	  if (this.y <= 0) {
+		 this.dezY = PlayerPosition.LEFT * Random.value();
+      }
+	}
+
+	this.intersects = function(player) {
+		if (player.position == PlayerPosition.LEFT) {
+		    if (player.x + player.width >= this.x &&
+		        player.y < this.y + this.height &&
+		        player.y + player.height > this.y) {
+			        Score.playerLeft++;
+				    return true;
+			}
+		} else if (player.position == PlayerPosition.RIGHT){
+		    if (player.x <= this.x + this.width &&
+				player.y < this.y + this.height &&
+				player.y + player.height > this.y) {
+					Score.playerRight++;
+					return true;
+			}
+		}
+		return false; 
+	}
+	
+	this.dezX = PlayerPosition.LEFT * Random.value();
+	this.dezY = PlayerPosition.RIGHT * Random.value();
 }
+Ball.prototype = new Drawable();
 
-function random(max){
- return parseInt(Math.random()*max);
-}
-
-function polarityRandom(){
-  var p=Math.random()*2;
-  if(p>1){ return 1;}
-  return -1;
-}
-
- function player1movement(evt){
-   lastKey1=evt.keyCode;
- }
-
- function player2movement(evt){
-   lastKey2=evt.keyCode;
- }
-
-
-document.addEventListener('keydown',player1movement,true);
-document.addEventListener('keydown',player2movement,true);
-
-
-function Rectangle(x,y,width,height,id){
- this.id=id;
- this.score=0;
- this.x=(x==null)?0:x;
- this.y=(y==null)?0:y;
- this.width=(width==null)?0:width;
- this.height=(height==null)?this.width:height;
-
- this.intersects=function(ball){
-   var ping=false;  
-   if(ball!=null){
-	  if(this.id==1){
-		 ping = this.x+this.width  >= ball.x &&
-			this.y < ball.y+ball.height &&
-		    this.y+this.height > ball.y;
-     } else if(this.id==2){
-    	 ping = this.x <= ball.x+ball.width &&
-			this.y < ball.y+ball.height &&
-		    this.y+this.height > ball.y;
-     }
-   }
-   return ping; 
- }
-
+/*also known as run()*/
+function animate() {
+    requestAnimFrame( animate );
+    game.paint();
+	game.player1.move();//Check intersect
+	game.player2.move();//Check intersect
+	game.pongBall.move();
+	game.pongBall.draw();
+	game.player1.draw();
+	game.player2.draw();
 }
